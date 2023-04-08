74d1b1e85aed34ca921b3c92adc66c6e98c1522a
devdhaif <devdhaif@gmail.com>
2023-04-08 18:31:44 +0200

    WIP

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,31 +1,16 @@
 <template>
-<!--    <div class="container px-6 py-6 mx-auto text-white bg-gray-700">-->
-
-<!--        <button class="bg-red-500" @click="show = ! show">-->
-<!--            {{show}}-->
-<!--            <slot>-->
-<!--                <p>{{Message}}</p>-->
-<!--            </slot> &lt;!&ndash; slot outlet &ndash;&gt;-->
-<!--        </button>-->
-<!--        <p class="bg-black text-white">userId : {{userId}}</p>-->
-
-<!--    </div>-->
-    <div class="p-4 bg-red-100 text-red-800 rounded-md shadow-md ">
-        <p>{{message}} {{editing}}</p>
+    <div @click="show = !show">
         <slot></slot>
     </div>
 </template>
 
 <script>
     export default {
-        props:['message' , 'editing'],
         data() {
             return {
-                show: false,
+                show: true
             }
-        },
-        mounted() {
-            console.log('Component mounted.')
         }
     }
+
 </script>
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,9 +1,13 @@
 @props(['reply'])
-<div id="reply-{{$reply->id}}" class="bg-white relative p-4 rounded shadow mt-4 flex justify-between gap-x-4 items-center">
+
+<div id="reply-{{$reply->id}}"
+     class="bg-white relative p-4 rounded shadow mt-4 flex justify-between gap-x-4 items-center">
+
+
 
     <div class="relative space-y-2 mt-4 p-4 rounded shadow bg-gray-50 flex-1 ">
         <a href="{{ route('profile.show' , $reply->owner) }}" class="text-sm  text-blue-600 ">
-                {{ $reply->owner->name }}
+            {{ $reply->owner->name }}
         </a>
         <span class="text-gray-600 text-sm pl-8">
 
@@ -18,32 +22,38 @@
                     <form class="" action="/replies/{{$reply->id}}" method="POST">
                         @csrf
                         @method('DELETE')
-                        <button class="bg-red-100 px-4 py-2 border border-red-500 text-red-800 hover:bg-red-200" type="submit"> Delete </button>
+                        <button class="bg-red-100 px-4 py-2 border border-red-500 text-red-800 hover:bg-red-200"
+                                type="submit"> Delete
+                        </button>
                     </form>
                 </div>
-                <example-component  message="{{$reply->body}}">
 
-                <div class=" ">
-                        <button @click="show = true" class="bg-green-100 px-4 py-6 border border-green-500 text-green-800 hover:bg-green-200" > Edit </button>
-                </div>
+                {{-- use  a vue 3 example component and access its data then add a button to toggle the data --}}
+                <example-component >
+                    <button class="bg-blue-100 px-4 py-2 border border-blue-500 text-blue-800 hover:bg-blue-200"
+                    > Edit
+                    </button>
                 </example-component>
 
-            <div>
+                <div>
 
-            </div>
+                </div>
             </div>
 
-            @endcan
+        @endcan
     </div>
 
     {{-- heart shape --}}
     <div class="flex items-center border rounded p-2">
         <form method="POST" action="/replies/{{ $reply->id }}/favorites">
             @csrf
-            <button type="submit" class="text-gray-600 text-sm disabled:text-gray-400" {{  $reply->isFavorited() ? 'disabled' : '' }}>
+            <button type="submit"
+                    class="text-gray-600 text-sm disabled:text-gray-400" {{  $reply->isFavorited() ? 'disabled' : '' }}>
                 {{ $reply->favorites_count }} {{ Str::plural('Favorite', $reply->favorites_count) }}
             </button>
         </form>
     </div>
 
+    <div v-if="show"> yes</div>
+    <div v-else> no</div>
 </div>
