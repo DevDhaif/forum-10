6b0dffb8b6243fc36313a662e662f6f1ff4b7075
devdhaif <devdhaif@gmail.com>
2024-04-30 14:02:27 +0300

    add admin middleware

---
+++ app/Http/Middleware/Admin.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class Admin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (!$request->user() || !$request->user()->hasRole('admin')) {
+            // Redirect non-authenticated or non-admin users to the home page
+            return redirect('/');
+        }
+
+        return $next($request);
+    }
+}
