c9a9d71ce269e9ccf2c2f407f738ef3ba585d72b
devdhaif <devdhaif@gmail.com>
2024-06-10 15:52:14 +0300

    redirect admin to questions

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -23,13 +24,18 @@ class AuthenticatedSessionController extends Controller
     /**
      * Handle an incoming authentication request.
      */
-    public function store(LoginRequest $request): RedirectResponse
+    public function store(LoginRequest $request)
     {
         $request->authenticate();
 
         $request->session()->regenerate();
+        if ($request->user()->hasRole('admin')) {
+            return redirect()->route('questions');
+        }
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+        return redirect()->route('threads');
+
+        // return redirect()->intended(RouteServiceProvider::HOME);
     }
 
     /**
